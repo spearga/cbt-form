906e79ede783d9b54c25f80e8289adfba215148c
G.spear <G@spear.ac>
2024-06-01 16:00:15 +0100

    Added lambda

---
+++ aws/terraform/lambda.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  inline_policy {
+    name = "s3-access-policy"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::cbt-gs/v1.0.0/lambda.zip"
+    }
+  ]
+}
+EOF
+  }
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "ServerlessExample"
+  s3_bucket     = "cbt-gs"
+  s3_key        = "v1.0.0/lambda.zip"
+  handler       = "main.handler"
+  runtime       = "nodejs18.x"
+  role          = aws_iam_role.lambda_exec.arn
+}
