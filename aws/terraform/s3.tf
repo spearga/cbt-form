c85953e798d3f282f8ec7567eb971c15ff6e5f40
G.spear <G@spear.ac>
2024-06-02 14:56:43 +0100

    move policy

--- aws/terraform/s3.tf
+++ aws/terraform/s3.tf
@@ -31,3 +31,20 @@ resource "aws_s3_bucket_public_access_block" "cbt_gs" {
   ignore_public_acls  = false
   restrict_public_buckets = false
 }
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.cbt_gs.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cbt_gs.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cbt_gs_policy" {
+  bucket = aws_s3_bucket.cbt_gs.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
